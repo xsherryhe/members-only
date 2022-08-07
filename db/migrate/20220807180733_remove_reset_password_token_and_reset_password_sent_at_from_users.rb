0f94d52709380904867dacb34715ba792fe451ab
Sherry He <xsherryhe@gmail.com>
2022-08-07 14:11:16 -0400

    Removed recoverable module and associated db columns

---
+++ db/migrate/20220807180733_remove_reset_password_token_and_reset_password_sent_at_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveResetPasswordTokenAndResetPasswordSentAtFromUsers < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :reset_password_token, :string
+    remove_column :users, :reset_password_sent_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_07_025908) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_07_180733) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -24,13 +24,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_07_025908) do
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
