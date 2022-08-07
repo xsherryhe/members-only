5e8f1c065b127028b20084c019b87e4799055dae
Sherry He <xsherryhe@gmail.com>
2022-08-07 14:10:30 -0400

    Filled out code for #new, #create, and #index actions and views of Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,27 @@
 class PostsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!, only: [:new, :create]
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
