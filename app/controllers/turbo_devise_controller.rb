b0953de1b96aadfdcfc7e64bbbb10c276e9a6fa1
Sherry He <xsherryhe@gmail.com>
2022-08-06 22:41:50 -0400

    Added authenticable User MVC using Devise gem

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -6,7 +6,7 @@ class TurboDeviseController < ApplicationController
       if get?
         raise error
       elsif has_errors? && default_action
-        render rendering_options.merge(formats: html, status: :unprocessable_entity)
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
       else
         redirect_to navigation_location
       end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :username, presence: true,
+                       uniqueness: true,
+                       length: { maximum: 20 }
+  validates :password, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?]).*\Z/,
+                                 message: "must contain at least 1 of each: " \
+                                          "uppercase letter, lowercase letter, digit, symbol" }
+
+  has_many :posts
+end
