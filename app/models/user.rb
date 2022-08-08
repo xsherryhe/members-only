727becfd10f243655a77ba4ed13650369e502c82
Sherry He <xsherryhe@gmail.com>
2022-08-07 20:40:30 -0400

    Fixed bug involving password validation on update account page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
                        length: { maximum: 20 }
   validates :password, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?]).*\Z/,
                                  message: "must contain at least 1 of each: " \
-                                          "uppercase letter, lowercase letter, digit, symbol" }
+                                          "uppercase letter, lowercase letter, digit, symbol" },
+                       unless: -> { password.nil? }
 
   has_many :posts
 end
